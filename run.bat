e8fd0dbe8651e55362985379c8ed978b179874c8
gamesun <gamesunyt@gmail.com>
2014-02-18 18:37:16 +0900

    init v1.0

---
+++ run.bat
@@ -0,0 +1,21 @@
+@echo off
+
+title repLabel v1.0
+
+set PATH=D:\sunyt\01_tool\strawberry\perl\bin;%PATH%
+
+if not  exist D:\sunyt\01_tool\strawberry\perl\bin\perl.exe (
+    echo Can not found the perl.exe.
+    echo.
+    pause
+    goto END
+)
+
+echo Welcome to repLabel v1.0
+echo.
+
+    cmd /k
+    
+goto END
+
+:END
---
+++ sample_new/test2.c
@@ -0,0 +1,12 @@
+
+func1_abc()
+{
+	x = func3_ghi();
+	x = func345();
+}
+
+
+func1234()
+{
+
+}
