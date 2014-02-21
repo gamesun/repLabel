4e7b80b162d44f557c7bcbea0795803cf0b7fc66
sun <none>
2014-02-21 12:41:52 +0900

    update to v1.1

--- run.bat
+++ run.bat
@@ -1,17 +1,8 @@
 @echo off
 
-title repLabel v1.0
+title repLabel v1.1
 
-set PATH=D:\sunyt\01_tool\strawberry\perl\bin;%PATH%
-
-if not  exist D:\sunyt\01_tool\strawberry\perl\bin\perl.exe (
-    echo Can not found the perl.exe.
-    echo.
-    pause
-    goto END
-)
-
-echo Welcome to repLabel v1.0
+echo Welcome to repLabel v1.1
 
     cmd /k repLabel.pl
     
